4ee4ee6e93ff7aa0323088ace43900e28695c6a7
Hoseuk <hoseuk0917@example.com>
2025-10-29 00:34:50 +0900

    fix: update .env and DB connection to internal URL

---
+++ utils/db.js
@@ -0,0 +1,11 @@
+// utils/db.js
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  close: () => pool.end(),
+};
